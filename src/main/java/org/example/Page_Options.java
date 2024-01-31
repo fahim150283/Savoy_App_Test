8700240751b5a86d93aaa88e9a64b1c3f5144243
Abul Hossain Chowdhury <fahim150283@gmail.com>
2024-01-31 11:58:40 +0600

    just a tiny adjustment

--- src/main/java/org/example/Page_Options.java
+++ src/main/java/org/example/Page_Options.java
@@ -3,21 +3,29 @@ package org.example;
 import io.appium.java_client.MobileElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.touch.TouchActions;
 
 import java.net.MalformedURLException;
 
 public class Page_Options extends BrowserUtils{
+    public static String xpath;
+    public static String id;
     public void Login(String username, String password) throws MalformedURLException, InterruptedException {
+        TouchActions touchaction = new TouchActions(driver);
+        System.out.println(username+" - "+password);
+
         //click the username and input in the field
-        MobileElement un = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]");
-        un.click();
-        driver.getKeyboard().sendKeys(username);
+        xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]";
+        MobileElement un = (MobileElement) driver.findElementByXPath(xpath);
+        touchaction.singleTap(un).perform();
+        new Actions(driver).sendKeys(username).perform();
 
         Thread.sleep(2000);
 
         //click the password and input in the field
         MobileElement pw = (MobileElement) driver.       findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]");
-        pw.click();
-        driver.getKeyboard().sendKeys(password);
+        touchaction.singleTap(pw).perform();
+        new Actions(driver).sendKeys(password).perform();
     }
 }
